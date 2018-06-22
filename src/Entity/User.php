686b1531503423157af475a945dfcca3a268e388
delboy1978uk <delboy1978uk@gmail.com>
2018-06-22 20:42:15 +0200

    add discriminator column

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,8 @@ use Doctrine\ORM\Mapping as ORM;
  * @package Del\Entity
  * @ORM\Entity(repositoryClass="Del\Repository\UserRepository")
  * @ORM\Table(name="User",uniqueConstraints={@ORM\UniqueConstraint(name="email_idx", columns={"email"})})
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="class", type="string")
  */
 class User extends BaseUser
 {
