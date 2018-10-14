ff97ed8e3f9f314249816556117fe3f7f49621bf
delboy1978uk <delboy1978uk@gmail.com>
2018-10-14 05:36:12 +0200

    change link expired exception message

--- src/Exception/EmailLinkException.php
+++ src/Exception/EmailLinkException.php
@@ -7,6 +7,6 @@ use Exception;
 class EmailLinkException extends Exception
 {
     const LINK_NOT_FOUND = 'A matching email link was not found';
-    const LINK_EXPIRED = 'A matching email link was not found';
+    const LINK_EXPIRED = 'The email link has expired.';
     const LINK_NO_MATCH = 'The token did not match.';
 }
