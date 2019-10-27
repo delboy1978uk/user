e4e41b87515185e75765ba279dfa5ab104a153c3
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-10-27 02:21:18 +0200

    use congig/db file

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'db' => [
+        'driver' => 'pdo_mysql',
+        'dbname' => 'delboy1978uk',
+        'user' => 'dbuser',
+        'password' => 'dbpass',
+        'host' => '127.0.0.1',
+    ],
+    'packages' => [
+        'delboy1978uk/person',
+    ],
+];
