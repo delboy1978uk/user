15f2437e93e6f37d6d49b2d5859a7321808a5727
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-01-25 01:41:37 +0100

    move bootstrap code

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,3 +1,13 @@
 <?php
-// This is global bootstrap for autoloading
-//require_once('vendor/autoload.php');
+
+use Del\Common\Config\DbCredentials;
+use Del\Common\ContainerService;
+use Del\UserPackage;
+
+$creds = require_once '.migrant';
+$dbCredentials = new DbCredentials($creds['db']);
+$userPackage = new UserPackage();
+$containerSvc = ContainerService::getInstance();
+$containerSvc->setDbCredentials($dbCredentials);
+$containerSvc->registerToContainer($userPackage);
+$containerSvc->getContainer();
