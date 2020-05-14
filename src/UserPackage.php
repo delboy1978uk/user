fe3da2be2f1d686035962479a7630baa94ab5be6
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-05-14 03:11:48 +0200

    use command registration package

--- src/UserPackage.php
+++ src/UserPackage.php
@@ -4,13 +4,15 @@ namespace Del;
 
 use Barnacle\EntityRegistrationInterface;
 use Barnacle\RegistrationInterface;
+use Bone\Console\CommandRegistrationInterface;
+use Del\Console\UserCommand;
 use Del\Person\PersonPackage;
 use Del\Person\Service\PersonService;
 use Del\Service\UserService;
 use Barnacle\Container;
 use Doctrine\ORM\EntityManager;
 
-class UserPackage implements RegistrationInterface, EntityRegistrationInterface
+class UserPackage implements RegistrationInterface, EntityRegistrationInterface, CommandRegistrationInterface
 {
     /**
      * @param Container $c
@@ -36,5 +38,17 @@ class UserPackage implements RegistrationInterface, EntityRegistrationInterface
     public function getEntityPath(): string
     {
         return 'vendor/delboy1978uk/user/src/Entity';
+    }
+
+    /**
+     * @param Container $container
+     * @return array
+     */
+    public function registerConsoleCommands(Container $container): array
+    {
+        $userService = $container->get(UserService::class);
+        $userCommand = new UserCommand($userService);
+        
+        return [$userCommand];
     }
 }
